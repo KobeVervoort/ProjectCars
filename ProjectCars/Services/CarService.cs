7ff6fe97aa158f71e6be6ce036572f167e96b966
KobeVervoort <kobevervoort@gmail.com>
2018-01-16 16:40:36 +0100

    Moved data queries from CarController to CarService

---
+++ ProjectCars/Services/CarService.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ProjectCars.Data;
+using ProjectCars.Entities;
+using ProjectCars.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+
+
+namespace ProjectCars.Services
+{
+    public class CarService : ICarService
+    {
+        private readonly CarsContext _carsContext;
+
+        public CarService(CarsContext carsContext)
+        {
+            _carsContext = carsContext;
+        }
+
+        private IIncludableQueryable<Car, Owner> GetFullGraph()
+        {
+            return _carsContext.Cars.Include(c => c.Version).Include(c => c.Owner);
+        }
+
+        public List<Car> GetAllCars()
+        {
+            return GetFullGraph().OrderBy(c => c.Id)
+                .ToList();
+        }
+
+        public Car GetCarById(int id)
+        {
+            return GetFullGraph()
+                .FirstOrDefault(c => c.Id == id);
+        }
+
+        public List<Owner> GetAllOwners()
+        {
+            return _carsContext.Owners.ToList();
+        }
+
+        public Owner GetOwnerById(int id)
+        {
+            return _carsContext.Owners.Find(id);
+        }
+
+        public List<Version> GetAllVersions()
+        {
+            return _carsContext.Versions.ToList();
+        }
+
+        public Version GetVersionById(int id)
+        {
+            return _carsContext.Versions.Find(id);
+        }
+
+        public void Persist(Car car)
+        {
+            if (car.Id == 0)
+                _carsContext.Cars.Add(car);
+            else
+                _carsContext.Cars.Update(car);
+            _carsContext.SaveChanges();
+        }
+
+        void Delete(int id)
+        {
+            var toDelete = GetCarById(id);
+            if (toDelete != null)
+            {
+                _carsContext.Cars.Remove(toDelete);
+                _carsContext.SaveChanges();
+            }
+        }
+
+        void ICarService.Delete(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ ProjectCars/Services/ICarService.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using ProjectCars.Entities;
+
+namespace ProjectCars.Services
+{
+    public interface ICarService
+    {
+        List<Car> GetAllCars();
+        Car GetCarById(int id);
+        List<Owner> GetAllOwners();
+        Owner GetOwnerById(int id);
+        List<Version> GetAllVersions();
+        Version GetVersionById(int id);
+        void Persist(Car car);
+        void Delete(int id);
+    }
+}
